6eb8bc572de4466b8f63e849eea727de82a7201e
grishav <pokharel36954@gmail.com>
2023-12-20 18:13:21 -0500

    complete program that consumes rest web service

---
+++ src/main/java/com/example/consumingrest/Value.java
@@ -0,0 +1,10 @@
+package com.example.consumingrest;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record Value(Long id, String quote ) {
+
+	
+	
+}
